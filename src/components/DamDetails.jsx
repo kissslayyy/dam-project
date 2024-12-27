afbb1a8b9982933b71dd1253996175860d547a95
kislay-gupta <kislaygupta007@gmail.com>
2024-12-27 12:04:05 +0530

    feat: added image

--- src/components/DamDetails.jsx
+++ src/components/DamDetails.jsx
@@ -1,74 +1,76 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './DamDetails.css';
+import { useNavigate } from "react-router-dom";
+import "./DamDetails.css";
 import logo from "../assets/logo.jpeg";
-
+import sumit from "../assets/sumit.jpeg";
+import mantesh from "../assets/mantesh.jpeg";
+import ompriya from "../assets/ompriya.jpeg";
+import vivek from "../assets/vivek.jpeg";
+import sumant from "../assets/sumant.jpeg";
 const DamDetails = () => {
   const navigate = useNavigate();
   const details = [
     {
       name: "Sumit kumar",
       regNo: "2201128907",
-      image: "https://xsgames.co/randomusers/avatar.php?g=male&i=1"
+      image: sumit,
     },
     {
       name: "Om priya",
       regNo: "21101128027",
-      image: "https://xsgames.co/randomusers/avatar.php?g=female&i=2"
+      image: ompriya,
     },
     {
       name: "Mamtesh kumar",
       regNo: "2110112826",
-      image: "https://xsgames.co/randomusers/avatar.php?g=male&i=3"
+      image: mantesh,
     },
     {
       name: "Vivek kumar",
       regNo: "21101128027",
-      image: "https://xsgames.co/randomusers/avatar.php?g=male&i=4"
+      image: vivek,
     },
     {
       name: "Sumant kumar",
       regNo: "21101128026",
-      image: "https://xsgames.co/randomusers/avatar.php?g=male&i=5"
-    }
+      image: sumant,
+    },
   ];
 
   return (
     <div className="container">
-      <button 
-        onClick={() => navigate('/')}
-        className="back-button"
-      >
+      <button onClick={() => navigate("/")} className="back-button">
         <span className="back-icon">←</span> Go to Home
       </button>
-      <img src={logo} alt="Logo" style={{ 
-        display: "block", 
-        margin: "0 auto",
-        width: "150px",
-        height: "auto",
-        
-      }} />
-       <h1 style={{ 
-        textAlign: "center",
-        fontSize: "calc(1.2rem + 1vw)",
-        marginBottom: "1rem",
-        padding: "0 10px"
-      }}>
-B. P. Mandal College of Engineering Madhepura Project</h1>
+      <img
+        src={logo}
+        alt="Logo"
+        style={{
+          display: "block",
+          margin: "0 auto",
+          width: "150px",
+          height: "auto",
+        }}
+      />
+      <h1
+        style={{
+          textAlign: "center",
+          fontSize: "calc(1.2rem + 1vw)",
+          marginBottom: "1rem",
+          padding: "0 10px",
+        }}
+      >
+        B. P. Mandal College of Engineering Madhepura Project
+      </h1>
       <h1 className="title">Team member Details</h1>
 
       <div className="grid-container">
         {details.map((detail, index) => (
           <div key={index} className="card">
             <div className="card-header">
-              <img 
-                src={detail.image} 
-                alt={detail.name}
-                className="avatar"
-              />
+              <img src={detail.image} alt={detail.name} className="avatar" />
               <h2 className="name">{detail.name}</h2>
             </div>
-            
+
             <div className="card-content">
               <div className="detail-row">
                 <span className="label">Registration No:</span>
